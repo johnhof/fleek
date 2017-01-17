04bba7bc0395ac71f3118d78c8159577accff820
johnhof <john.hofrichter@gmail.com>
2017-01-16 17:02:18 -0800

    added parser

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports = {
+  parser: require('fleek-parser'),
   context: require('fleek-context'),
   validator: require('fleek-validator'),
   router: require('fleek-router')
--- tests/smoke.js
+++ tests/smoke.js
@@ -9,4 +9,5 @@ describe('Fleek', () => {
   it('should include fleek-context', () => expect(fleek.context).to.be.ok);
   it('should include fleek-validator', () => expect(fleek.validator).to.be.ok);
   it('should include fleek-router', () => expect(fleek.router).to.be.ok);
+  it('should include fleek-parser', () => expect(fleek.parser).to.be.ok);
 });
